3e183106d5300bde16f03ed847425380eba52679
Sue Tarazi <suemachtley@gmail.com>
2020-05-12 17:34:14 -0700

    added summary comments

--- XamarinAppiOS/Classes/ARtest.cs
+++ XamarinAppiOS/Classes/ARtest.cs
@@ -14,8 +14,13 @@ namespace XamarinAppiOS.Classes
     {
         private readonly ARSCNView sceneView;
 
+        /// <summary>
+        /// this creates a blank scene to ensure the camera feature of the mobile device is activated
+        /// </summary>
+        /// <param name="handle">variable name given to connect with the iOS API</param>
         public ViewController(IntPtr handle) : base(handle)
         {
+
             this.sceneView = new ARSCNView
             {
                 AutoenablesDefaultLighting = true,
@@ -26,6 +31,9 @@ namespace XamarinAppiOS.Classes
             this.View.AddSubview(this.sceneView);
         }
 
+        /// <summary>
+        /// abstract class that hands down the ViewDidLoad() for child methods to inherit
+        /// </summary>
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
@@ -33,6 +41,10 @@ namespace XamarinAppiOS.Classes
             this.sceneView.Frame = this.View.Frame;
         }
 
+        /// <summary>
+        /// Boolean method for animation and adding text to the scene
+        /// </summary>
+        /// <param name="animated">animated is either true or false</param>
         public override void ViewDidAppear(bool animated)
         {
             base.ViewDidAppear(animated);
@@ -45,15 +57,19 @@ namespace XamarinAppiOS.Classes
                 WorldAlignment = ARWorldAlignment.Gravity
             }, ARSessionRunOptions.ResetTracking | ARSessionRunOptions.RemoveExistingAnchors);
 
-            // Lesson: Add text to scene
+            //Add text to scene
             var textNode = new TextNode("Hello Universe", UIColor.Orange)
             {
-                Position = new SCNVector3(0, -0.6f, 0) // Any lower than this and it doesn't show!?
+                Position = new SCNVector3(0, -0.6f, 0) // Specifies position that the 3D text appears. Any lower than this and it doesn't show.
             };
 
             this.sceneView.Scene.RootNode.AddChildNode(textNode);
         }
 
+        /// <summary>
+        /// This boolean method is the opposite of the previous method and allows the view to disappear if bool is true.
+        /// </summary>
+        /// <param name="animated">animated is either true or false </param>
         public override void ViewDidDisappear(bool animated)
         {
             base.ViewDidDisappear(animated);
@@ -61,14 +77,23 @@ namespace XamarinAppiOS.Classes
             this.sceneView.Session.Pause();
         }
 
+        /// <summary>
+        /// This method handles error messaging
+        /// </summary>
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
         }
     }
 
+   
     public class TextNode : SCNNode
-    {
+    {   
+        /// <summary>
+        /// This method is called from the ViewDidAppear method above 
+        /// </summary>
+        /// <param name="text">string of text</param>
+        /// <param name="color">color of text</param>
         public TextNode(string text, UIColor color)
         {
             var rootNode = new SCNNode
@@ -80,6 +105,12 @@ namespace XamarinAppiOS.Classes
             AddChildNode(rootNode);
         }
 
+        /// <summary>
+        /// Geometry to create the 3D effect
+        /// </summary>
+        /// <param name="text">string text</param>
+        /// <param name="color">color of text</param>
+        /// <returns>3D text in specified color</returns>
         private static SCNGeometry CreateGeometry(string text, UIColor color)
         {
             var geometry = SCNText.Create(text, 0.01f);
@@ -89,9 +120,6 @@ namespace XamarinAppiOS.Classes
             geometry.FirstMaterial.Diffuse.Contents = color;
             geometry.FirstMaterial.Specular.Contents = UIColor.Blue;
             return geometry;
-        }
-        class ARtest
-        {
         }
     }
 }
